15e85f1b3491b896591867161839953a08bf743d
Marcus Kinsella <mckinsel@exon.(none)>
2010-04-06 10:24:28 -0700

    Change FASTQ sorting so it looks for sets of four lines rather than specific record separators because, of course, ther RS could change from file to file. Recall that @ is not a goo separator because ord('@') - 33 = 31 , and this is a perfectly reasonable quality score. So @ could appear in quality strings.

--- sortfastq.sh
+++ sortfastq.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-awk 'BEGIN {RS = "\n@ENST"} {gsub(/\n/, "~~", $0); print "@ENST" $0}' $1 | sort -S 4G | sed -e 's/~~/\n/g' -e 's/\n\n/\n/g'  > $1.sorted;
+
+awk ' BEGIN { cntr = 1 }
+{
+	if ( cntr < 4 ) {
+		printf("%s~~", $0)
+	}
+	if ( cntr == 4 ) {
+		printf("%s\n", $0)
+		cntr = 0
+	}
+	cntr++
+}
+END {
+	rest = int(cntr)
+	if ( rest != 1 ) {
+		printf("\n")
+	}
+}
+' $1 | sort -T . -S 4G | sed -e 's/~~/\n/g' -e 's/\n\n/\n/g'  > $1.sorted;
