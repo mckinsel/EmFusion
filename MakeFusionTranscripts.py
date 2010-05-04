e7d6d560ec961dd083aa79b7fb9f70f0794bb374
mckinsel <mckinsel@localhost>
2010-05-04 16:42:31 -0700

    Keep track of the fusion point in the name of the fusion.

--- MakeFusionTranscripts.py
+++ MakeFusionTranscripts.py
@@ -35,14 +35,15 @@ def main(exon_seq_file, fusion_exons_file):
     
     count = 0
     for line in file(fusion_exons_file):
+        count += 1
         seq = ""
         ls = line.strip().split('\t')
-        for exon in ls[1:]:
+        for exon in ls[2:]:
             seq += esq.getseq(exon)
-        outf.write('>F_' + ls[0] + '_' + str(count) + '|F_' + str(count) +'|fusion|fusions|0|100|1\n')
+        outf.write('>F_' + ls[0] + '_' + ls[1] + '_' + str(count) + '|F_' + str(count) +'|fusion|fusions|0|100|1\n')
         outf.write(seq)
         outf.write('\n')
-        count += 1
+        
         
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2])
