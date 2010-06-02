7bf708fcfb66a6e03a9fa849b7e559c1b16a9502
mckinsel <mckinsel@localhost>
2010-06-02 15:57:12 -0700

    Make parseBlat handle psl outputs rather than Blast.

--- parseBlat.py
+++ parseBlat.py
@@ -1,16 +1,49 @@
-from Bio.Blast import NCBIStandalone
+#!/usr/bin/python
+
 import sys
 
+class psl_iterator(object):
+    
+    def __init__(self, pslfile):
+        self._file = open(pslfile, 'r')
+        
+    def __iter__(self):
+        return self
+    
+    def next(self):
+        nl = self._file.readline()
+        
+        
+        if len(nl) == 0:
+            return None
+        else:
+            nl_list = nl.strip('\n').split('\t')
+            while len(nl_list) < 15 or nl_list[0] == 'match':
+                nl = self._file.readline()
+                nl_list = nl.strip('\n').split('\t')
+        
+        match_count = int(nl_list[0])
+        mismatch_count = int(nl_list[1])
+        read_id = nl_list[9]
+        mapping = nl_list[13]
+        mapping_start = int(nl_list[15])
+        mapping_end = int(nl_list[16])
+        read_length = int(nl_list[10])
+        
+        return {'match_count':match_count, 'mismatch_count':mismatch_count,
+                'read_id':read_id, 'mapping':mapping, 'mapping_start':mapping_start,
+                'mapping_end':mapping_end}
+        
+        
 
 def main(blat_results_file, max_distance = '15000'):
-    bf = open(blat_results_file, 'r')
-    bp = NCBIStandalone.BlastParser()
-    bi = NCBIStandalone.Iterator(bf, bp)
 
+    bi = psl_iterator(blat_results_file)
+    
     max_dist = int(max_distance)
 
-    read_alns_1 = {}
-    read_alns_2 = {}
+    read_alns_1 = {} ##Dictionary from read_id to list of hits
+    read_alns_2 = {} ##Same for other paired end
     
     counter = 0
     while 1:
@@ -22,20 +55,24 @@ def main(blat_results_file, max_distance = '15000'):
         
         counter += 1
 
-        read_id = rec.query
+        read_id = rec['read_id']
+
+        ##This is kind of specific to the format of your read ids
+        ##end shoudl be '1' or '2'
+        ##read_id should end with '/1' or '/2'
+
         end = read_id[-1]
         base_read_id = read_id[:-2]
-        read_length = rec.query_letters
+        read_length = rec['read_length']
 
         if base_read_id not in eval('read_alns_' + end):
             eval('read_alns_' + end)[base_read_id] = []
 
-        for aln in rec.alignments:
-            mapping = aln.title
 
-            for hit in aln.hsps:
-                if hit.identities[0] >= .9 * read_length:
-                    eval('read_alns_' + end)[base_read_id].append((mapping, hit.sbjct_start))
+        mapping = rec['mapping']
+
+        if rec['match_count'] >= .8 * read_length:
+            eval('read_alns_' + end)[base_read_id].append((mapping, rec['mapping_start']))
     
     for read in read_alns_1:
         maps1 = read_alns_1[read]
